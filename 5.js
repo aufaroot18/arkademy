b6cd5490db4b15f53836d88275c294aff70d0e3e
Aufa Billah <aufaroot18@gmail.com>
2019-05-04 19:45:06 +0700

    update no 5: looping menampilkan array

--- 5.js
+++ 5.js
@@ -2,12 +2,22 @@ const karakterAcak = (input) => {
 	const character = "0123456789abcdefghijklmnopqrstuvwxyz";
 	const data = input;
 	var result = '';
+	var results = [];
+
 	for (let i=0; i<data; i++) {
+		// untuk membuat karakter acak
 		for (let j=0; j<32; j++) {
 			result += character.charAt(Math.random() * 32);
 		}
-		console.log(result);
+
+		// untuk melakukan pengecekan
+		results.push(result);
 		result = "";
+	}
+
+	// untuk menampilkan karakter menggunakan perulangan
+	for (let i=0; i<results.length; i++) {
+		console.log(results[i]);
 	}
 }
 
